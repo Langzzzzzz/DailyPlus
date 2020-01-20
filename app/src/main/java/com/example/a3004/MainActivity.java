86139196b5608b7febcd1ef645ec4312d2b65104
Payton Dong Pei <silence1198@outlook.com>
2020-01-19 20:51:52 -0500

    first launch

--- app/src/main/java/com/example/a3004/MainActivity.java
+++ app/src/main/java/com/example/a3004/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a3004;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //update information
-        //again
+        firstLaunch();
     }
+
+    private void firstLaunch() {
+        SharedPreferences sharedPreferences = getSharedPreferences("FirstRun",0);
+        Boolean first_run = sharedPreferences.getBoolean("First",true);
+        if (first_run){
+            sharedPreferences.edit().putBoolean("First",false).apply();
+            Toast.makeText(this,"First Launch",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this,"Not First Launch",Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
