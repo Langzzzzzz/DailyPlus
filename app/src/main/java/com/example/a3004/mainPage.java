556b2a55093b7194d415d213f79d7fc82fadfa62
Payton Dong Pei <silence1198@outlook.com>
2020-02-01 14:30:50 -0500

    loading pic

---
+++ app/src/main/java/com/example/a3004/mainPage.java
@@ -0,0 +1,14 @@
+package com.example.a3004;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
