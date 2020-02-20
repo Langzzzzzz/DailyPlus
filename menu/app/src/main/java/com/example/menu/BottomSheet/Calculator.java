b6a4911682dd23f7857592d86a9c822ecdf0247d
Dong Pei <dongpei@dhcp-42-252.cu-wireless-faculty.carleton.ca>
2020-02-20 18:13:10 -0500

    social login now works
    still need improvement

---
+++ menu/app/src/main/java/com/example/menu/BottomSheet/Calculator.java
@@ -0,0 +1,47 @@
+package com.example.menu.BottomSheet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.menu.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class Calculator extends BottomSheetDialogFragment {
+
+    private Button mButton1;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+    private Button mButton5;
+    private Button mButton6;
+    private Button mButton7;
+    private Button mButton8;
+    private Button mButton9;
+    private Button mButton0;
+    private Button mButtonDot;
+    private Button mButtonMin;
+    private Button mButtonAdd;
+    private Button mButtonDel;
+    private Button mButtonEnt;
+
+//    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.bottom_sheet_calculator);
+////    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.bottom_sheet_calculator, container,false);
+            return v;
+    }
+}
+
