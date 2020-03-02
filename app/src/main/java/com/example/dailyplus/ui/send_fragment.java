57a193752638c4efbd36067d12a1103dd28fd752
Zhenqing Lang <zhenqinglang@cmail.carleton.ca>
2020-03-02 15:02:53 -0500

    user feedback done
    user feedback done

--- app/src/main/java/com/example/dailyplus/ui/send_fragment.java
+++ app/src/main/java/com/example/dailyplus/ui/send_fragment.java
@@ -1,9 +1,13 @@
 package com.example.dailyplus.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +16,33 @@ import androidx.fragment.app.Fragment;
 import com.example.dailyplus.R;
 
 public class send_fragment extends Fragment {
+    private Button send;
+    private TextView emailAddress, emailBody;
+    private String emailSubject = "User Feedback";
+    private String SemailBody;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_send, container,false);
+        send = view.findViewById(R.id.FeedbacksendButton);
+        emailAddress = view.findViewById(R.id.useremail);
+        emailBody = view.findViewById(R.id.userfeedback);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailURI = "mailto:" + emailAddress.getText().toString();
+                SemailBody = emailBody.getText().toString();
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(emailURI));
+
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+                emailIntent.putExtra(Intent.EXTRA_TEXT, SemailBody);
+
+                startActivity(Intent.createChooser(emailIntent, "Email Client ..."));
+            }
+        });
 
         return view;
     }
+
+
 }
