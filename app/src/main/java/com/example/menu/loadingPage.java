7ab5919e15d521352b69d9076e5b96a64e8040a7
HaiyangNi <haiyangni@cmail.carleton.ca>
2020-03-19 23:06:57 -0400

    delete Commented code

--- app/src/main/java/com/example/menu/loadingPage.java
+++ app/src/main/java/com/example/menu/loadingPage.java
@@ -37,7 +37,7 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class  loadingPage extends AppCompatActivity {
     public static int SLASH = 3000;//waiting time
-//    private CallbackManager mCallbackManager;
+
     private FirebaseApp mAuth = FirebaseApp.getInstance();
     FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
     private EditText passwordInput;
@@ -51,48 +51,11 @@ public class  loadingPage extends AppCompatActivity {
         password = db.queryPassword();
         setContentView(R.layout.loading);
         passwordInput = findViewById(R.id.password);
-//        // Initialize Facebook Login button
-//        mCallbackManager = CallbackManager.Factory.create();
-//        LoginButton loginButton = findViewById(R.id.login_button);
-//        loginButton.setReadPermissions("email", "public_profile");
-//        loginButton.registerCallback(mCallbackManager, new
-//                FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                Log.d("Success", "facebook:onSuccess:" + loginResult);
-////                handleFacebookAccessToken(loginResult.getAccessToken());
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                Log.d("Cancel", "facebook:onCancel");
-//                // ...
-//            }
-//
-//            @Override
-//            public void onError(FacebookException error) {
-//                Log.d("Error", "facebook:onError", error);
-//                // ...
-//            }
-//        });
-
-// ...
 
         Button login = findViewById(R.id.jumpButton);
         login.setOnClickListener(v->
         {
-//            if (password.equals("") && !passwordInput.getText().toString().equals("")) {
-//                Toast.makeText(this, passwordInput.getText().toString(), Toast.LENGTH_LONG).show();
-//                try {
-//                    PrintWriter writer = new PrintWriter(file);
-//                    writer.println(passwordInput.getText());
-//                    writer.flush();
-//                    writer.close();
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//                Toast.makeText(this, password, Toast.LENGTH_LONG).show();
+
             if (passwordInput.getText().toString().equals(password) || password.equals("")) {
                 Intent nextScreen = new Intent(loadingPage.this, MainActivity.class);
                 nextScreen.putExtra("user", "default");
@@ -103,41 +66,20 @@ public class  loadingPage extends AppCompatActivity {
             }
         });
 
-//        firstLaunch();
-//        new Handler().postDelayed(() -> {
-//            Intent nextScreen = new Intent(loadingPage.this, MainActivity.class);
-//            startActivity(nextScreen);
-//            finish();
-//            //should load first pic
-//        }, SLASH);
     }
-//
-//    private void firstLaunch() {
-//        SharedPreferences sharedPreferences = getSharedPreferences("FirstRun",0);
-//        Boolean first_run = sharedPreferences.getBoolean("First",true);
-//        if (first_run){
-//            sharedPreferences.edit().putBoolean("First",false).apply();
-//            //first run => launch tutorial
-//        }
-//        //else start app as usuals
-//
-//    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-    // Pass the activity result back to the Facebook SDK
-//        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
     @Override
     public void onStart() {
         super.onStart();
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-
         updateUI(currentUser);
     }
+
     public void updateUI(FirebaseUser currentUser){
-        //do something with user then
         Toast.makeText(loadingPage.this,"You just login",Toast.LENGTH_LONG).show();
     }
 
